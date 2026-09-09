e60ee2bc7ccad16b65f7b6194c969c3b4b2ded87
root <root@marcos.localdomain>
2026-09-09 10:58:07 -0300

    atualizado o application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,13 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  helper_method :usuario_logado
+
+  def usuario_logado
+    if session[:user_id]
+      @usuario_logado = User.find_by(id: session[:user_id])
+    end
+    @usuario_logado
+  end
 end
